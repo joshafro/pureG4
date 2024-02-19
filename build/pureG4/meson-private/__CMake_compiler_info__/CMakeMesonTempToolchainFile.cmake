4668e373e65ee5e1733079c3e5d02b99003e7299
joshafro <160252295+joshafro@users.noreply.github.com>
2024-02-19 20:19:46 +0000

    Add files via upload

---
+++ build/pureG4/meson-private/__CMake_compiler_info__/CMakeMesonTempToolchainFile.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_SIZEOF_VOID_P "8")
+set(CMAKE_CXX_COMPILER "/nix/store/a4smsbc3bl8mh64cdfbidhbccjwa5n8z-clang-wrapper-16.0.1/bin/clang++")
---
+++ build/pureG4/meson-private/cmake_Geant4/CMakeFiles/3.25.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.1.0-1033-oem")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.1.0-1033-oem")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/josh/medphys/pureG4/build/pureG4/meson-private/cmake_Geant4/CMakeMesonToolchainFile.cmake")
+
+set(CMAKE_SYSTEM "Linux-6.1.0-1033-oem")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "6.1.0-1033-oem")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "FALSE")
+
+set(CMAKE_SYSTEM_LOADED 1)
